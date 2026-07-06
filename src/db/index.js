89f8f4a343857ceb488f8d42ba6d94d797668411
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2026-07-06 16:55:09 +0530

    first commit

--- src/db/index.js
+++ src/db/index.js
@@ -1,20 +1,23 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  "crm",
-  "root",
-  "Root@12345",
-  {
-    host: "localhost",
-    port: 3306,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize("crm", "root", "Root@12345", {
+  host: "127.0.0.1",
+  port: 3306,
+  dialect: "mysql",
+  logging: console.log,
+});
 
-const db = {
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Database connected");
+  } catch (err) {
+    console.error("❌ Connection error:");
+    console.error(err);
+  }
+})();
+
+export default {
   sequelize,
   Sequelize,
 };
-
-export default db;
